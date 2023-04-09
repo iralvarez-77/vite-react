eaee101bb373844665d5897c7198d559cf5b0d11
Ira Álvarez <irainelly.alvarez@comafi.com.ar>
2023-04-08 21:54:27 -0300

    effect is added

--- App.jsx
+++ App.jsx
@@ -1,28 +1,53 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-
-const FACT_URL = 'https://catfact.ninja/fact'
-// const IMAG_CAT_URL = `https://cataas.com/cat/says/${}`
+const FACT_URL = 'https://catfact.ninja/fact';
+const PREFIX_URL = 'https://cataas.com';
 
 const App = () => {
-
-  const [fact, setFact] = useState()
-  const [imag, setImag] = useState()
-
+  
+  const [fact, setFact] = useState();
+  const [imag, setImag] = useState();
+  
   useEffect(() => {
-
     fetch(FACT_URL)
       .then(resp => resp.json())
       .then(data => setFact(data.fact))
-  }, [])
+  }, []);
+
+  useEffect(() => {
+
+    if (!fact ) return;
+
+    const threeWrods = fact.split(' ', 3).join(' ');
+    fetch(`https://cataas.com/cat/says/${threeWrods}?size=50&color=red&json=true`)
+      .then( resp => resp.json())
+      .then(resp => {
+    const { url } = resp;
+    setImag(url);
+      })
+  },[fact]);
 
   return (
-    <main>
-      <h1>App de gatos</h1>
-      {fact && <p>{fact}</p>}
+    <main style={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      maxWidth: '800px',
+      margin: '0 auto',
+    }}>
+      <h1>Aplicación de gatos</h1>
+      <section style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        gap:'16px'
+      }}>
+        {fact && <p>{fact}</p>}
+        {imag && <img style={{ maxWidth:'320px', height: 'auto'}} src={`${PREFIX_URL}${imag}`} alt="imagen de gatos con las 3 primeras palabras" />}
+      </section>
     </main>
   )
-
 }
 
 export default App
